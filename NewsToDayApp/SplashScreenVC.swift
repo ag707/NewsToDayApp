de97ed12562b74c298a171531d653d1ee9aef0d8
Amsterdance26 <amsterdance26@gmail.com>
2023-05-10 17:24:59 +0700

    added SplashScreenVC

---
+++ NewsToDayApp/SplashScreenVC.swift
@@ -0,0 +1,29 @@
+//
+//  SplashScreenVC.swift
+//  NewsToDayApp
+//
+//  Created by Василий Васильевич on 10.05.2023.
+//
+
+import UIKit
+
+class SplashScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
